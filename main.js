5425a1db5c22af34558f2a89091edd7939167a8d
Brian4755 <brian.mccune1@yahoo.com>
2024-01-16 23:20:27 -0600

    code console log and comment changes

--- main.js
+++ main.js
@@ -18,13 +18,8 @@ camera.position.setZ(30);
 
 
 const geometry = new THREE.SphereGeometry(7,50);
-// const geometry = new THREE.BoxGeometry(15,15,15);
 const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true});
-const material1 = new THREE.MeshBasicMaterial( {color: 0x0000FF, wireframe: true});
 const sphere = new THREE.Mesh(geometry, material);
-const sphere1 = new THREE.Mesh(geometry, material1);
-const sphere2 = new THREE.Mesh(geometry, material);
-const sphere3 = new THREE.Mesh(geometry, material);
 
 
 document.addEventListener('mousemove', onDocumentMouseMove)
@@ -36,14 +31,9 @@ function onDocumentMouseMove(e) {
   mouseX = e.clientX - window.innerWidth/2
   mouseY = e.clientY - window.innerHeight/2
 }
-// const controls = new OrbitControls(camera, renderer.domElement);
-
-
-console.log(changeSize())
 
 let sizeGoal = 0
 let event = false
-let sign = 1
 
 function shrink() {
   sizeGoal = 0
@@ -51,7 +41,7 @@ function shrink() {
   if (event === false) {
     Math.random() > 0.9 ? event = true : event
   }
-  console.log(event)
+  // console.log(event)
 
   if (event === true) {
     if (sizeGoal === 0) {
@@ -63,29 +53,13 @@ function shrink() {
         event = !event
       }
     }
-    // sizeGoal = -1 * sign
-
-    // if (sphere.scale.x <= 0.01 && sizeGoal === -1) {
-    //   sizeGoal = 0
-    //   event = !event
-    //   // sign *= -1
-    // } else if (sphere.scale.x <= 0.01) {
-    //   sizeGoal = 3
-    // }
   }
-  //   sizeGoal += -1
-  // if (sphere.scale.x < 0.01) {
-  //   sizeGoal += 2
-  // }
-  // if (sphere.scale.x > 1.1) {
-
-  // }
-  console.log(sphere.scale.x, sizeGoal)
+  // console.log(sphere.scale.x, sizeGoal)
   setTimeout(shrink, 2000)
 }
 
 
-console.log(sphere.scale)
+// console.log(sphere.scale)
 
 function animate() {
   requestAnimationFrame(animate);
@@ -94,8 +68,6 @@ function animate() {
   sphere.lookAt(target)
   sphere.position.x += (mouseX * 0.12 - sphere.position.x) * 0.02
   sphere.position.y += (-mouseY * 0.12 - sphere.position.y) * 0.02
-  sphere1.position.x += (mouseX * 0.12 - sphere1.position.x) * 0.02
-  sphere1.position.y += (-mouseY * 0.2 - sphere1.position.y) * 0.04
   
 
   sphere.scale.x += sizeGoal * 0.01
@@ -111,7 +83,7 @@ function animate() {
 shrink()
 
 
-scene.add(sphere, sphere1);
+scene.add(sphere);
 
 renderer.render(scene, camera);
 animate()
