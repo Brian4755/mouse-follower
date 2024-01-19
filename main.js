86641b0b521fff92c8931de5552ec0e369cc0650
Brian4755 <brian.mccune1@yahoo.com>
2024-01-18 22:33:36 -0600

    percentage animation change

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ const waterHeightMap = textureLoader.load("./public/Water_002_DISP.png");
 const waterRoughness = textureLoader.load("./public/Water_002_ROUGH.jpg");
 const waterAmbientOcclusion = textureLoader.load("./public/Water_002_OCC.jpg");
 
-const target = new THREE.Vector3
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector3();
 
 const scene = new THREE.Scene();
 
@@ -35,9 +36,6 @@ dirLight.castShadow = true
 scene.add(dirLight)
 
 const geometry = new THREE.SphereGeometry(4.5,50);
-// const material = new THREE.MeshBasicMaterial({
-//   map: waterBaseColor,
-// });
 const material = new THREE.MeshStandardMaterial({
   map: waterBaseColor,
   normalMap: waterNormalMap, 
@@ -47,19 +45,16 @@ const material = new THREE.MeshStandardMaterial({
 });
 const sphere = new THREE.Mesh(geometry, material);
 
-// sphere.receiveShadow = true
-// sphere.castShadow = true
-
-document.addEventListener('mousemove', onDocumentMouseMove)
 controls.enableZoom = false;
 
-let mouseX = 0
-let mouseY = 0
 
-function onDocumentMouseMove(e) {
-  mouseX = e.clientX - window.innerWidth/2
-  mouseY = e.clientY - window.innerHeight/2
+function onPointerMove( event ) {
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
+
+pointer.unproject(camera)
+pointer.sub(camera.position).normalize()
 
 let sizeGoal = 0
 let event = false
@@ -68,9 +63,8 @@ function shrink() {
   sizeGoal = 0
 
   if (event === false) {
-    Math.random() > 0.8 ? event = true : event
+    Math.random() > 0.95 ? event = true : event
   }
-  // console.log(event)
 
   if (event === true) {
     if (sizeGoal === 0) {
@@ -83,29 +77,26 @@ function shrink() {
       }
     }
   }
-  console.log( mouseX, sphere.position.x)
-  // console.log(sphere.scale.x, sizeGoal)
   setTimeout(shrink, 2000)
 }
 
 
-// console.log(sphere.scale)
 
 function animate() {
   requestAnimationFrame(animate);
   sphere.rotation.z += 0.01
-  target.x = (mouseX);
-  target.y = (-mouseY);
-  sphere.position.x += (mouseX * 0.119 - sphere.position.x) * 0.03
-  sphere.position.y += (-mouseY * 0.119 - sphere.position.y) * 0.03
   
+  raycaster.setFromCamera( pointer, camera );
 
-  // sphere.scale.x += sizeGoal * 0.01
-  // sphere.scale.y += sizeGoal * 0.01
-  // sphere.scale.z += sizeGoal * 0.01
-  // if (event === true && sphere.scale.x < 0.01) {
-  //   sphere.scale.set(0,0,0)
-  // }
+  sphere.scale.x += sizeGoal * 0.01
+  sphere.scale.y += sizeGoal * 0.01
+  sphere.scale.z += sizeGoal * 0.01
+  if (event === true && sphere.scale.x < 0.01) {
+    sphere.scale.set(0,0,0)
+  }
+
+    console.log(pointer.x, sphere.position.x)
+
 
   controls.update();
 
@@ -116,6 +107,8 @@ shrink()
 
 
 scene.add(sphere);
+
+window.addEventListener( 'pointermove', onPointerMove );
 
 renderer.render(scene, camera);
 animate()
